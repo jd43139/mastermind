277b5edcc56e7f4197a99bce916cba5e6e8e04da
jd43139 <109301496+jd43139@users.noreply.github.com>
2024-09-18 12:07:10 +0400

    The computer can be the code breaker. The Player can choose his/her rule (code generator or code breaker). The rules will be swaped after a complete session.

--- mastermind.rb
+++ mastermind.rb
@@ -1,10 +1,3 @@
-class Array
-  def find_all_indexes(value)
-    indexes = []
-    self.each_index{|i| indexes.push(i) if self[i] == value}
-    indexes
-  end
-end
 class MasterMind
 
   def generate_code
@@ -13,7 +6,7 @@ class MasterMind
     computer_code.map { digits.sample }
   end
 
-  def digits_included?(guess,code)
+  def give_feedback(guess,code)
     at_exact_position = 0
     not_at_exact_position = 0
     guess.each_with_index do |digit,index|
@@ -28,23 +21,56 @@ class MasterMind
   def isValidGuess?(guess)
     guess.length == 4 && guess.all? { |digit| digit.is_a?(Integer)}
   end
-  
-
 end
-mastermind = MasterMind.new
-secret_code = mastermind.generate_code
-p secret_code
-guess = nil
-until guess == secret_code
-  puts "Guess The Four Digits Code\n"
-  guess = gets.chomp.split('').map{ |item| item.to_i }
-  until mastermind.isValidGuess?(guess)
-    puts "Error: Please Enter Four Digits"
+
+def code_breaker
+  puts "You are the code breaker"
+  mastermind = MasterMind.new
+  secret_code = mastermind.generate_code
+  guess = nil
+  trials = 12
+  until guess == secret_code || trials.zero?
     puts "Guess The Four Digits Code\n"
+    puts "You have #{trials} trials"
     guess = gets.chomp.split('').map{ |item| item.to_i }
+    until mastermind.isValidGuess?(guess)
+      puts "Error: Please Enter Four Digits"
+      puts "Guess The Four Digits Code\n"
+      guess = gets.chomp.split('').map{ |item| item.to_i }
+    end
+    trials -=1
+
+    puts mastermind.give_feedback(guess, secret_code)
   end
 
-  puts mastermind.digits_included?(guess, secret_code)
+  p "The secret code is #{ secret_code}"
+  code_generator
 end
 
+def code_generator
+  puts "Your the code generator"
+  puts "Please Enter Your Secret Code"
+  secret_code = gets.chomp.split('').map{ |item| item.to_i }
+  trials = 0
+  computer_guess = nil
+  player_feedback = "n"
+  until player_feedback == 'y' || trials == 12
+    computer_guess = MasterMind.new.generate_code
+    p computer_guess
+    puts "Does the guess matches your code y/n #{trials}"
+    player_feedback = gets.chomp
+    trials+=1
+    sleep 0.5
+  end
+  puts "done in #{trials} trials"
+  code_breaker
+end
+puts "Please choose your rule (x-code breaker or y-code generator) x/y"
+choice = gets.chomp
 
+
+if choice == 'x'
+  code_breaker
+else
+  code_generator
+end
